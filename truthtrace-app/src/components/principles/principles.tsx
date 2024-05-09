8212721ac3a6809e15ad6cb24f2c799acaa86118
Nara Humano <nhumano207@alumno.unlam.edu.ar>
2024-05-08 23:46:44 -0300

    feat: se sube cambios de la landing page

--- truthtrace-app/src/components/principles/principles.tsx
+++ truthtrace-app/src/components/principles/principles.tsx
@@ -3,11 +3,16 @@ import './principles.css';
 const Principles = () => (
     <div className="principles">
         <div className="label">
-      <p className="titulo-falopa">
-        <span className="text-wrapper">Nos preocupamos por tu </span>
-        <span className="span">seguridad</span>
-      </p>
-    </div>
+            <p className="titulo-falopa">
+                <span className="text-wrapper">Nos preocupamos por tu </span>
+                <span className="span">seguridad</span><br/><br/>
+                <span className="text-wrapper">En TruthTrace nos dedicamos a proporcionar soluciones que brinden </span>
+                <span className="span">transparencia, seguridad y accesibilidad </span>
+                <span className="text-wrapper">
+                en tus envíos. <br />
+                </span>
+            </p>
+        </div>
     </div>
 )
 
