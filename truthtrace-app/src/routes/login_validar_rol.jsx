5317d90ed78aeaf9d73f7898bcf3507c501fa6f8
Nara Humano <nhumano207@alumno.unlam.edu.ar>
2024-05-10 18:22:35 -0300

    feat: diseño de validacion de roles para login y register

--- truthtrace-app/src/routes/login_validar_rol.jsx
+++ truthtrace-app/src/routes/login_validar_rol.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import src from '../assets/images/logo.svg';
 
 
 const Login = () => (
-  <div>
+  <div className='validar'>
+    <a href='#home' ><img src={src} alt="logo"  /></a>
     <h1>Bienvenido</h1>
-    <h2>Roles</h2>
-    <Link to="/login/empresa">Empresa</Link>
-    <Link to="/login/transportista">Transportista</Link>
+    <h2>¿Cuál es tu rol?</h2>
+    <Link to="/login/empresa" className='button-empresa'>Empresa</Link>
+    <Link to="/login/transportista" className='button-usuario'>Transportista</Link>
   </div>
 );
 
