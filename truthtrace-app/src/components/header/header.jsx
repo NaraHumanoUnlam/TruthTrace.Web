bc4469d3dc0f08517ce70ed3270ccd81187a9b35
Paula Assini <paulaassini04@gmail.com>
2024-05-10 19:16:05 -0300

    Crea el hero y organiza los archivos de la landing page

--- truthtrace-app/src/components/header/header.jsx
+++ truthtrace-app/src/components/header/header.jsx
@@ -5,12 +5,12 @@ import './header.css';
 const Header = ({ src }) => (
     <header className="header" id='home'>
         <nav className='menu'>
-        <a href='#home' className="header__logo"><img src={src} alt="logo"  /></a>
+            <a href='#home' className="header__logo"><img src={src} alt="logo"/></a>
             <ul className="header__menu">
                 <li className="header__menu-item"><a href='#home'>Home</a></li>
                 <li className="header__menu-item"><a href='#about'>Sobre nosotros</a></li>
-                <li className="header__menu-item"><a href='#valores'>Valores TruthTrace</a></li>
-                <li className="header__menu-item"><Link to="/Login">Login</Link></li>
+                <li className="header__menu-item"><a href='#valores'>Valores</a></li>
+                <li className="header__menu-item"><Link to="/Login">Ingresar</Link></li>
             </ul>
         </nav>
     </header>
--- truthtrace-app/src/routes/home.jsx
+++ truthtrace-app/src/routes/home.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import './styles.css';
-import logo from '../assets/images/logo.png'; 
 import imagen from '../assets/images/portada-about.svg';
-import Header from '../components/header/header';
 import Principles from '../components/principles/principles';
 import About from '../components/about/about.jsx';
+import Hero from '../components/hero/hero.jsx';
 
 const Home = () => (
-    <div className="main">
-        <Header src={logo}/>
-        <h1 className='slogan'>Seguridad en cada envío,<br/> confianza en cada entrega</h1>
-        <h2 className='slogan2'>Descubre la diferencia con blockchain</h2>
-       <Principles /> 
-       <About img={imagen} />
+    <div>
+        <Hero />
+        <Principles /> 
+        <About img={imagen} />
     </div>
 )
 
