c1ce8fd7d43e29857d4429129155501129277982
Nara Humano <nhumano207@alumno.unlam.edu.ar>
2024-05-11 02:05:34 -0300

    feat: adaptacion de logo en el login segun rol

--- truthtrace-app/src/components/login/login.jsx
+++ truthtrace-app/src/components/login/login.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
+import empresaLogo from '../../assets/images/empresa/logo.svg';
+import usuarioLogo from '../../assets/images/usuario/logo.svg';
 import './login.css';
-const Login = ({ clase }) => (
+
+const Login = ({ clase }) => {
+  const logoSrc = clase === 'empresa' ? empresaLogo : usuarioLogo;
+  return (
   <div className={clase}>
   <div className='desing'>
-  <img src={logo} alt="logo" />
+  <img src={logoSrc} alt="logo" />
   </div>
-    <div className='container'><a href='/#home'><img src={logo} alt="logo" /></a>
+    <div className='container'><a href='/#home'><img src={logoSrc} alt="logo" /></a>
     <h1>Bienvenido</h1>
     <form action="">
       <input type="text" name="email" placeholder="Email" />
@@ -18,6 +22,6 @@ const Login = ({ clase }) => (
     </div>
   </div>
 
-);
+)};
 
 export default Login;
--- truthtrace-app/src/routes/home.jsx
+++ truthtrace-app/src/routes/home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './styles.css';
-import imagen from '../assets/images/portada-about.svg';
 import Principles from '../components/principles/principles';
 import About from '../components/about/about.jsx';
 import Hero from '../components/hero/hero.jsx';
 import Contact from '../components/contact/contact.jsx';
 import Footer from '../components/footer/footer.jsx'
 import logo from '../assets/images/logo.png';
+import imagen from '../assets/images/logo.svg';
+
 
 const Home = () => (
     <div>
