dc2ed08ecfd09aca660f68ecf91d5e0f04b973b6
Paula Assini <paulaassini04@gmail.com>
2024-05-12 00:24:21 -0300

    Agrega imagen al login

--- truthtrace-app/src/components/login/login.jsx
+++ truthtrace-app/src/components/login/login.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import empresaLogo from '../../assets/images/empresa/logo.svg';
 import usuarioLogo from '../../assets/images/usuario/logo.svg';
+import empresaImg from '../../assets/images/empresa/person-computer-purple.svg';
 import './login.css';
 
 const Login = ({ clase }) => {
   const logoSrc = clase === 'empresa' ? empresaLogo : usuarioLogo;
+  const img = clase === 'empresa' ? empresaImg : empresaImg;
   return (
   <div className={clase}>
-  <div className='desing'>
-  <img src={logoSrc} alt="logo" />
-  </div>
+    <div className='desing'>
+      <img src={img} alt="logo" />
+    </div>
     <div className='container'>
       <a href='/#home'><img src={logoSrc} alt="logo" /></a>
       <h1>Bienvenido</h1>
